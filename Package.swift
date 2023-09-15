ffb52a9f60b33ec8f4922820bf978f0b24f025b0
matthieu <60342216+mattalbus@users.noreply.github.com>
2023-09-15 17:25:22 +0200

    update to Realm 10.42.2

--- Package.swift
+++ Package.swift
@@ -3,30 +3,29 @@
 
 import PackageDescription
 
-let realmVersion = "10.40.2"
+let realmVersion = "10.42.2"
 
 func buildTargets() -> [Target] {
-    let baseURL = "https://github.com/mattalbus/RealmBinaries/releases/download/\(realmVersion)"
+    let realmUrl = "https://github.com/realm/realm-swift/releases/download/v10.42.2/Realm.spm.zip"
+    let realmChecksum = "b3eee3236de1991cfe6ab3072052c33e4030c01154216bfab94224b2ca92b848"
 
-#if swift(>=5.8.0)
-    let xcodeVersion = "14_3_1"
-    let realmChecksum = "7f229ed97b9df12b1d37649a0e77a4617ad3b088a10701bdfeba16395fd112bf"
-    let realmSwiftChecksum = "5b1977889d208e9694c27769f856e9c711acc500b856bc9e386dd8754788175f"
+#if swift(>=5.9.0)
+    let realmSwiftUrl = "https://github.com/realm/realm-swift/releases/download/v10.42.2/RealmSwift@15.0.spm.zip"
+    let realmSwiftChecksum = "228b7d129e946cb1a839bd31ac07d4b3713dd89bca01b92b6abccb4756b16a80"
 #else
-    let xcodeVersion = "14_2_0"
-    let realmChecksum = "6063e93f88b4859180aa7e5ee21dfdca4bff2b4effec287c262b976fcb90018b"
-    let realmSwiftChecksum = "b4050758f13be33bc5b95e852f1641e574705c6f18a0a9f8feff7a712aaab6f4"
+    let realmSwiftUrl = "https://github.com/realm/realm-swift/releases/download/v10.42.2/RealmSwift@14.3.1.spm.zip"
+    let realmSwiftChecksum = "d230e9246837d30d4cca6777013e6c30eed69cc18fe8b302533d5e4c165edafa"
 #endif
     
     return [
         .binaryTarget(
             name: "Realm",
-            url: "\(baseURL)/Realm_\(xcodeVersion).xcframework.zip",
+            url: realmUrl,
             checksum: realmChecksum
         ),
         .binaryTarget(
             name: "RealmSwift",
-            url: "\(baseURL)/RealmSwift_\(xcodeVersion).xcframework.zip",
+            url: realmSwiftUrl,
             checksum: realmSwiftChecksum
         )
     ]
